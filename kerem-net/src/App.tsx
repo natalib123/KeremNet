09cbda46b1a524b5172fe813bcf817d6dcd671df
Natali Liberchuk <natalilib06@gmail.com>
2025-06-23 18:44:17 +0300

    Added fetching from json server for now

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import keremNet from './keremnet.png';
 import Post from './Post/post';
 
 function App() {
-  let publisher = 'Natali';
-  let postContent = 'My FIRST Post';
-  let postComments = ['niceee', 'coolll', '6 out of 6!'];
-  let postLikes = 6;
-  let postDate = new Date();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+    .catch(error => console.error('Error fetching data'))
+  });
+
   return (
     <div className="App">
       <header>
         <img src= {keremNet}/>
         <h1>Welcome to Kerem Net!</h1>
       </header>
-      <Post publisherName={publisher} 
-            content={postContent} 
-            comments={postComments}
-            likes={postLikes}
-            publishDate={postDate}
-      />
     </div>
   );
 }
