b46e16b5a2fdac766a094645a8d5c1adb301ec56
Natali Liberchuk <natalilib06@gmail.com>
2025-06-23 15:09:07 +0300

    Created post component class

--- kerem-net/src/Post/post.tsx
+++ kerem-net/src/Post/post.tsx
@@ -1,1 +1,15 @@
 import React from "react";
+
+interface PostInfo{
+    publisherName: string;
+    content: string;
+    comments: string[];
+    likes: number;
+    publishDate: Date;
+};
+
+const Post: React.FC = (props: PostInfo) => {
+    return(<div></div>);
+};
+
+export default Post;
