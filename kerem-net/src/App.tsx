a421669b98880e954ed6859f9c73aee4f17c290e
Natali Liberchuk <natalilib06@gmail.com>
2025-06-24 15:56:11 +0300

    Added home page tsx file

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -5,15 +5,6 @@ import keremNet from './Assets/keremnet.png';
 import Post from './Components/Post/Post';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/posts')
-    .then(response => response.json())
-    .then(data => setPosts(data))
-    .catch(error => console.error('Error fetching data'))
-  });
-
   return (
     <div className="App">
       <header>
---
+++ kerem-net/src/HomePage.tsx
@@ -0,0 +1,33 @@
+import React , {useState, useEffect} from 'react';
+import Post from './Components/Post/Post';
+
+const HomePage: React.FC = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+    .catch(error => console.error('Error fetching data'))
+  });
+
+  return (
+    <div className="Home">
+      <body>
+        <ul>
+            {posts.map(post => (
+              <li key={post["id"]}>
+                <Post publisherName= {post["publisherName"]} 
+                content= {post["postContent"]} 
+                publishDate= {new Date(post["postDate"])}
+                likes= {Number(post["postLikes"])}
+                comments= {post["postComments"]}/>
+              </li>
+            ))}
+        </ul>
+      </body>
+    </div>
+  );
+}
+
+export default HomePage;
