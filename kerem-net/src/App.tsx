fe1b784078238ae348ddb61d74fcb1217d7d85f3
Natali Liberchuk <natalilib06@gmail.com>
2025-06-25 17:48:51 +0300

    Added navigation bar

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'; 
 import './App.css';
-import Header from './Components/ApplicationLayout/Header';
+import Header from './Components/ApplicationLayout/Header/Header';
+import NavigationBar from './Components/ApplicationLayout/NavigationBar/NavigationBar';
 import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <NavigationBar/>
       <HomePage/>
     </div>
   );
--- kerem-net/src/Components/ApplicationLayout/Header/Header.tsx
+++ kerem-net/src/Components/ApplicationLayout/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import keremNet from '../../Assets/keremnet.png';
+import keremNet from '../../../Assets/keremnet.png';
+import './Header.css'
 
 const Header: React.FC = () => {
     return(
     <header>
-        <img src= {keremNet}/>
+        <img className="logo" src= {keremNet}/>
         <h1>Welcome to Kerem Net!</h1>
     </header>
     );
