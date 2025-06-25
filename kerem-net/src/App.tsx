c2f0d0ea30f31c177f5f415cf9b95680f8b55de5
Natali Liberchuk <natalilib06@gmail.com>
2025-06-25 18:36:08 +0300

    Seperated the Application layout to its own component

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'; 
 import './App.css';
-import Header from './Components/ApplicationLayout/Header/Header';
-import NavigationBar from './Components/ApplicationLayout/NavigationBar/NavigationBar';
+import AppLayout from './Components/ApplicationLayout/AppLayout';
 import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <NavigationBar/>
+      <AppLayout/>
       <HomePage/>
     </div>
   );
